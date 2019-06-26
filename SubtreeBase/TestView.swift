81cf9a00cf85daa0fb9cf76b99c70cf168b0dfc0
fank <locke_fank@126.com>
2019-06-26 12:04:53 +0800

    init

---
+++ SubtreeBase/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  SubtreeBase
+//
+//  Created by fank on 2019/4/26.
+//  Copyright © 2019 fank. All rights reserved.
+//
+
+import UIKit
+
+public class TestView: UIView {
+
+    public class func sayHello() {
+        print("**********")
+    }
+    
+    public class func mainProjectModify() {
+        print("mainProjectModify")
+    }
+}
